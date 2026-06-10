2b00b160e1fa1c4e1389750a92720e45f7ff704d
Adam Power <adamthedash2@proton.me>
2026-06-10 16:22:47 +0100

    Adapter docs

--- src/adapter.rs
+++ src/adapter.rs
@@ -17,6 +17,9 @@ use crate::{
 
 /// Tail-call adapters for combinators
 pub trait ParserAdapter<Input>: Parser<Input> + Sized {
+    /// Apply an infallible function to the output.
+    ///
+    /// See [`Map`] for more info.
     fn map<F, O>(self, func: F) -> Map<Self, F>
     where
         F: FnMut(Self::Output) -> O,
@@ -25,6 +28,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Map::new(self, func)
     }
 
+    /// Apply an infallible function to the output without adding to the trace.
+    ///
+    /// See [`MapSilent`] for more info.
     fn map_silent<F, O>(self, func: F) -> MapSilent<Self, F>
     where
         F: FnMut(Self::Output) -> O,
@@ -33,6 +39,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         MapSilent::new(self, func)
     }
 
+    /// Apply a fallible function to the output.
+    ///
+    /// See [`TryMap`] for more info.
     fn try_map<F, O, E>(self, func: F) -> TryMap<Self, F>
     where
         F: FnMut(Self::Output) -> std::result::Result<O, E>,
@@ -42,6 +51,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         TryMap::new(self, func)
     }
 
+    /// Restore input position on failure.
+    ///
+    /// See [`Checkpoint`] for more info.
     fn checkpoint(self) -> Checkpoint<Self>
     where
         Input: Copy,
@@ -49,6 +61,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Checkpoint::new(self)
     }
 
+    /// Look ahead without consuming input.
+    ///
+    /// See [`Peek`] for more info.
     fn peek(self) -> Peek<Self>
     where
         Input: Copy,
@@ -56,10 +71,16 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Peek::new(self)
     }
 
+    /// Repeat the parser a fixed number of times.
+    ///
+    /// See [`RepeatArray`] for more info.
     fn repeat<const N: usize>(self) -> RepeatArray<Self, [Self::Output; N]> {
         RepeatArray::new(self)
     }
 
+    /// Repeat the parser a runtime-determined number of times.
+    ///
+    /// See [`RepeatVec`] for more info.
     fn repeat_vec<C>(self, count: C) -> RepeatVec<Self, C>
     where
         C: ForwardRefGet,
@@ -68,6 +89,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         RepeatVec::new(self, count)
     }
 
+    /// Apply the parser repeatedly until it fails.
+    ///
+    /// See [`Many`] for more info.
     fn many(self) -> Many<Self>
     where
         Input: Copy,
@@ -75,10 +99,16 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Many::new(self)
     }
 
+    /// Store the parser's output in a `ForwardRef`.
+    ///
+    /// See [`Store`] for more info.
     fn store(self) -> Store<Self, Self::Output> {
         Store::new(self)
     }
 
+    /// Conditionally run the parser based on a boolean value.
+    ///
+    /// See [`Cond`] for more info.
     fn run_if<C>(self, cond: C) -> Cond<C, Self>
     where
         C: ForwardRefGet<Value = bool>,
@@ -86,6 +116,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Cond::new(cond, self)
     }
 
+    /// Create a parser that can be externally enabled or disabled.
+    ///
+    /// See [`Configured`] for more info.
     fn configured<C>(self, cond: C) -> (Configured<Self>, impl Fn())
     where
         C: ForwardRefGet<Value = bool>,
@@ -96,6 +129,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         (parser, conf)
     }
 
+    /// Run the parser, then execute a side-effect closure.
+    ///
+    /// See [`Configuring`] for more info.
     fn configuring<F>(self, configurator: F) -> Configuring<Self, F>
     where
         F: Fn(),
@@ -103,6 +139,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Configuring::new(self, configurator)
     }
 
+    /// Optionally apply the parser.
+    ///
+    /// See [`Opt`] for more info.
     fn optional(self) -> Opt<Self>
     where
         Input: Copy,
@@ -110,6 +149,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Opt::new(self)
     }
 
+    /// Validate the parser's output with a predicate.
+    ///
+    /// See [`Verify`] for more info.
     fn verify<F>(self, func: F) -> Verify<Self, F>
     where
         F: FnMut(&Self::Output) -> bool,
@@ -117,14 +159,23 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Verify::new(self, func)
     }
 
+    /// Add a user-friendly name to the parser's trace.
+    ///
+    /// See [`Trace`] for more info.
     fn trace(self, name: impl Into<String>) -> Trace<Self> {
         Trace::new(self, name)
     }
 
+    /// Hide the parser's internal details from the annotation tree.
+    ///
+    /// See [`TraceOpaque`] for more info.
     fn trace_opaque(self, name: impl Into<String>) -> TraceOpaque<Self> {
         TraceOpaque::new(self, name)
     }
 
+    /// Select a parser from a tuple by index.
+    ///
+    /// See [`Dispatch`] for more info.
     fn dispatch<D>(self, discriminant: D) -> Dispatch<D, Self>
     where
         Self: SameParserTuple<Input>,
@@ -133,6 +184,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Dispatch::new(discriminant, self)
     }
 
+    /// Repeat the parser with different parameter values each time.
+    ///
+    /// See [`Parameterize`] for more info.
     fn parameterize<V, S>(self, parameters: V, param_input: S) -> Parameterize<S, V, Self>
     where
         V: Parameters,
@@ -141,6 +195,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Parameterize::new(parameters, param_input, self)
     }
 
+    /// Parse a value surrounded by left and right delimiters.
+    ///
+    /// See [`Surrounded`] for more info.
     fn surrounded_by<L, R>(self, left: L, right: R) -> Surrounded<L, Self, R>
     where
         L: Parser<Input>,
@@ -149,6 +206,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Surrounded::new(left, self, right)
     }
 
+    /// Parse a value surrounded by the same delimiter on both sides.
+    ///
+    /// See [`SurroundedSymmetrical`] for more info.
     fn surrounded_by_sym<O>(self, outer: O) -> SurroundedSymmetrical<Self, O>
     where
         O: Parser<Input>,
@@ -156,6 +216,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         SurroundedSymmetrical::new(self, outer)
     }
 
+    /// Validate that the parser's output is not empty.
+    ///
+    /// See [`Verify`] for more info.
     fn non_empty<T>(self) -> Verify<Self, impl FnMut(&Self::Output) -> bool>
     where
         Self: Parser<Input, Output = Vec<T>>,
@@ -163,6 +226,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         self.verify(|values| !values.is_empty())
     }
 
+    /// Parse a fixed number of elements separated by a delimiter.
+    ///
+    /// See [`SeparatedArray`] for more info.
     fn separated_arr<const N: usize, S>(
         self,
         separator: S,
@@ -173,6 +239,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         SeparatedArray::new(separator, self)
     }
 
+    /// Parse a runtime-determined number of elements separated by a delimiter.
+    ///
+    /// See [`SeparatedVec`] for more info.
     fn separated_vec<S, C>(self, separator: S, count: C) -> SeparatedVec<Self, S, C>
     where
         S: Parser<Input>,
@@ -182,6 +251,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         SeparatedVec::new(separator, self, count)
     }
 
+    /// Parse a tuple of parsers with separators between them.
+    ///
+    /// See [`SeparatedTuple`] for more info.
     fn separated_tuple<S>(self, separator: S) -> SeparatedTuple<S, Self>
     where
         Self: ParserTuple<Input>,
@@ -190,6 +262,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         SeparatedTuple::new(separator, self)
     }
 
+    /// Parse a prefix, then return the result of the keeper parser.
+    ///
+    /// See [`Preceded`] for more info.
     fn ignore_then<P>(self, keep: P) -> Preceded<Self, P>
     where
         P: Parser<Input>,
@@ -197,6 +272,9 @@ pub trait ParserAdapter<Input>: Parser<Input> + Sized {
         Preceded::new(self, keep)
     }
 
+    /// Parse a keeper, then a suffix, returning the keeper's result.
+    ///
+    /// See [`Terminated`] for more info.
     fn then_ignore<P>(self, ignore: P) -> Terminated<P, Self>
     where
         P: Parser<Input>,
